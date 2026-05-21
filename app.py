f82a4bc494ba606295a98cb74f03b17bee1023cf
24eg107d61-create <24eg107d61@anurag.edu.in>
2026-05-21 14:47:28 +0530

    Rename slider and update predicted marks variable

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import streamlit as st
 
 st.title("Predictive Modeling Using Machine Learning")
 
-hours = st.slider("Study Hours", 1, 10)
+hours = st.slider("Enter Study Hours", 1, 10)
 
-marks = hours * 10
+predicted_marks = hours * 10
 
-st.write("Predicted Marks:", marks)
+st.write("Predicted Marks:", predicted_marks)
+
+st.success("Machine Learning Prediction Successful")
